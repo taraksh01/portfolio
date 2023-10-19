39a51c8cefa4ef337e6f94d327481eec039d71a2
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-10-19 21:16:54 +0530

    Moved content from Projects component to Home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -23,6 +23,28 @@ const Home = () => {
           </p>
         </div>
       </article>
+      <article className="my-10 text-white font-mono">
+        <div className="flex justify-between my-10">
+          <div className="flex items-center w-2/3">
+            <h2 className="text-4xl">
+              <span className="text-orange-400">#</span>projects
+            </h2>
+            <div className="w-2/3 h-px mx-5 bg-orange-400 flex-shrink-0"></div>
+          </div>
+          <button className="text-xl">
+            view all <span>{"-->"}</span>
+          </button>
+        </div>
+        <div className="flex justify-between gap-6">
+          <Project
+            tech={["React", "tailwind", "parcel"]}
+            name={"project 1"}
+            desc={"This is a short description for project"}
+            live={"https://github.com/taraksh01/pet-pooja"}
+            code={"https://github.com/taraksh01/pet-pooja"}
+          />
+        </div>
+      </article>
     </main>
   );
 };
