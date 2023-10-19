c2722193415bb29c0f02d5c1e66c787e81b211f0
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-10-19 17:13:19 +0530

    Rendered Home component on app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./Components/Header";
+import Home from "./Components/Home";
 
 const App = () => {
   return (
-    <>
+    <div className="bg-black/95">
       <Header />
-    </>
+      <Home />
+    </div>
   );
 };
 
