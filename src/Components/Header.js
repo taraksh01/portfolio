51f4f693e07512aab2aa696e771c5e3deae752cd
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-10-19 12:15:53 +0530

    Created Header for the web app

---
+++ src/Components/Header.js
@@ -0,0 +1,27 @@
+const Header = () => {
+  return (
+    <>
+      <header className="max-w-screen-xl h-14 mx-auto bg-black/95 text-gray-100 flex items-end justify-between font-mono px-3">
+        <h1 className="text-2xl font-semibold cursor-pointer text-white">
+          Tarak Shaw
+        </h1>
+        <nav className="flex gap-6">
+          <p className="text-lg cursor-pointer hover:text-white active:text-white">
+            <span className="text-orange-400">#</span>home
+          </p>
+          <p className="text-lg cursor-pointer hover:text-white active:text-white">
+            <span className="text-orange-400">#</span>projects
+          </p>
+          <p className="text-lg cursor-pointer hover:text-white active:text-white">
+            <span className="text-orange-400">#</span>about
+          </p>
+          <p className="text-lg cursor-pointer hover:text-white active:text-white">
+            <span className="text-orange-400">#</span>contact
+          </p>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
