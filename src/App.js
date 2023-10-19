a68538be0947c3a2aa33ab479b764d650912297a
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-10-19 20:27:27 +0530

    Added Projects section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./Components/Header";
 import Home from "./Components/Home";
+import Projects from "./Components/Projects";
 
 const App = () => {
   return (
     <div className="bg-black/95">
       <Header />
       <Home />
+      <Projects />
     </div>
   );
 };
