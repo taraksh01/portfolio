ab583306e81261243e815b5f2b2608eb75e36029
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-10-19 20:24:37 +0530

    Added SingleProject component on projects section to show top projects preview

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,7 +1,9 @@
+import Project from "./SingleProject";
+
 const Projects = () => {
   return (
-    <article className="max-w-screen-xl mx-auto text-white font-mono">
-      <div className="flex justify-between">
+    <article className="max-w-screen-xl mx-auto m-10 text-white font-mono">
+      <div className="flex justify-between my-10">
         <div className="flex items-center w-2/3">
           <h2 className="text-4xl">
             <span className="text-orange-400">#</span>projects
@@ -11,6 +13,15 @@ const Projects = () => {
         <button className="text-xl">
           view all <span>{"-->"}</span>
         </button>
+      </div>
+      <div className="flex justify-between gap-6">
+        <Project
+          tech={["React", "tailwind", "parcel"]}
+          name={"project 1"}
+          desc={"This is a short description for project"}
+          live={"https://github.com/taraksh01/pet-pooja"}
+          code={"https://github.com/taraksh01/pet-pooja"}
+        />
       </div>
     </article>
   );
