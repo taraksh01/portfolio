52ba19775d80725dfec1ab4b987acc5b92264659
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-10-20 20:12:49 +0530

    Added hover and active psudo state color to link elements

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,30 +3,28 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <header className="max-w-screen-xl h-14 mx-auto text-gray-100 flex items-end justify-between font-mono">
-        <Link to={"/"}>
-          <h1 className="text-2xl font-semibold cursor-pointer text-white">
-            Tarak Shaw
-          </h1>
+      <header className="max-w-screen-xl h-14 mx-auto text-gray-200 flex items-end justify-between font-mono">
+        <Link to={"/"} className="hover:text-white active:text-white">
+          <h1 className="text-2xl font-semibold">Tarak Shaw</h1>
         </Link>
         <nav className="flex gap-6">
-          <Link to={"/"}>
-            <p className="text-xl cursor-pointer hover:text-white active:text-white">
+          <Link to={"/"} className="hover:text-white active:text-white">
+            <p className="text-xl">
               <span className="text-orange-400">#</span>home
             </p>
           </Link>
-          <Link to={"/projects"}>
-            <p className="text-xl cursor-pointer hover:text-white active:text-white">
+          <Link to={"/projects"} className="hover:text-white active:text-white">
+            <p className="text-xl cursor-pointer">
               <span className="text-orange-400">#</span>projects
             </p>
           </Link>
-          <Link to={"/about"}>
-            <p className="text-xl cursor-pointer hover:text-white active:text-white">
+          <Link to={"/about"} className="hover:text-white active:text-white">
+            <p className="text-xl">
               <span className="text-orange-400">#</span>about
             </p>
           </Link>
-          <Link to={"/contact"}>
-            <p className="text-xl cursor-pointer hover:text-white active:text-white">
+          <Link to={"/contact"} className="hover:text-white active:text-white">
+            <p className="text-xl">
               <span className="text-orange-400">#</span>contact
             </p>
           </Link>
