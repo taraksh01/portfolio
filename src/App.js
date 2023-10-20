5c1d176eb03c27ec9dc0e6d6adc6dd75b95e788a
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-10-20 19:56:27 +0530

    Added routing to the app

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Projects from "./Components/Projects";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="bg-black/95">
       <Header />
-      <Home />
+      <Outlet />
       <Footer />
     </div>
   );
 };
 
+const appConfig = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/projects", element: <Projects /> },
+      // { path: "/about", element: <About /> },
+      // { path: "/contact", element: <Contact /> },
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<RouterProvider router={appConfig} />);
