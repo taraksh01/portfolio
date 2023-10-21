303ec46bde495bc67f3c25be227848c1cb3f8b75
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-10-21 12:06:32 +0530

    Reduced padding from project name, desc and tech

--- src/Components/SingleProject.js
+++ src/Components/SingleProject.js
@@ -3,15 +3,15 @@ const Project = ({ tech, name, desc, live, code }) => {
     <>
       <div className="">
         <img className="h-64 w-full rounded-xl" src="" alt="" />
-        <p className="text-lg p-2">{tech.join(", ")}</p>
         <p className="text-3xl font-semibold p-2">{name}</p>
-        <p className="text-lg p-2">{desc}</p>
-        <div className="flex gap-4 p-2">
-          <button className="text-lg border border-orange-300 p-2 rounded-md">
-            live {"<-->"}
+        <p className="text-lg p-1">{tech.join(", ")}</p>
+        <p className="text-lg p-1">{desc}</p>
+        <div className="flex gap-4 p-1 xl:py-2">
+          <button className="text-lg border border-orange-300 p-1 rounded-md">
+            live {"🚀"}
           </button>
-          <button className="text-lg border border-orange-300 p-2 rounded-md">
-            code {"🚀"}
+          <button className="text-lg border border-orange-300 p-1 rounded-md">
+            code {"</>"}
           </button>
         </div>
       </div>
