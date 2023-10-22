428f9b71ff0077a0948fec051cc2573c5054e911
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-10-22 11:37:40 +0530

    Made projects page with dummy projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,28 +2,67 @@ import Project from "./SingleProject";
 
 const Projects = () => {
   return (
-    <article className="max-w-screen-xl mx-auto m-10 text-white font-mono">
-      <div className="flex justify-between my-10">
-        <div className="flex items-center w-2/3">
-          <h2 className="text-4xl">
-            <span className="text-orange-400">#</span>projects
+    <main className="max-w-screen-xl mx-auto my-10 text-white flex flex-col font-mono">
+      <h2 className="text-3xl md:text-4xl mx-4 md:mx-2 my-3">
+        <span className="text-orange-400">/</span>projects
+      </h2>
+      <p className="mx-4 md:mx-2 sm:text-lg">List of my projects</p>
+      <div className="mx-4 md:mx-2">
+        <div className="my-10">
+          <h2 className="text-2xl md:text-3xl">
+            <span className="text-orange-400">#</span>complete-apps
           </h2>
-          <div className="w-2/3 h-px mx-5 bg-orange-400 flex-shrink-0"></div>
         </div>
-        <button className="text-xl">
-          view all <span>{"-->"}</span>
-        </button>
+        <div className="sm:mx-4 flex flex-col justify-center items-center sm:flex-row sm:flex-wrap gap-6 sm:justify-start">
+          <Project
+            tech={["React", "tailwind", "parcel"]}
+            name={"project 1"}
+            desc={"This is a short description for project"}
+            live={"https://github.com/taraksh01/pet-pooja"}
+            code={"https://github.com/taraksh01/pet-pooja"}
+          />
+          <Project
+            tech={["React", "tailwind", "parcel"]}
+            name={"project 1"}
+            desc={"This is a short description for project"}
+            live={"https://github.com/taraksh01/pet-pooja"}
+            code={"https://github.com/taraksh01/pet-pooja"}
+          />
+        </div>
       </div>
-      <div className="flex justify-between gap-6">
-        <Project
-          tech={["React", "tailwind", "parcel"]}
-          name={"project 1"}
-          desc={"This is a short description for project"}
-          live={"https://github.com/taraksh01/pet-pooja"}
-          code={"https://github.com/taraksh01/pet-pooja"}
-        />
+      <div className="mx-4 md:mx-2">
+        <div className="my-10">
+          <h2 className="text-2xl md:text-3xl">
+            <span className="text-orange-400">#</span>in-progress
+          </h2>
+        </div>
+        <div className="sm:mx-4 flex flex-col justify-center items-center sm:flex-row sm:flex-wrap gap-6 sm:justify-start">
+          {/* <Project
+            tech={["React", "tailwind", "parcel"]}
+            name={"project 1"}
+            desc={"This is a short description for project"}
+            live={"https://github.com/taraksh01/pet-pooja"}
+            code={"https://github.com/taraksh01/pet-pooja"}
+          /> */}
+        </div>
       </div>
-    </article>
+      <div className="mx-4 md:mx-2">
+        <div className="my-10">
+          <h2 className="text-2xl md:text-3xl">
+            <span className="text-orange-400">#</span>mini-projects
+          </h2>
+        </div>
+        <div className="sm:mx-4 flex flex-col justify-center items-center sm:flex-row sm:flex-wrap gap-6 sm:justify-start">
+          {/* <Project
+            tech={["React", "tailwind", "parcel"]}
+            name={"project 1"}
+            desc={"This is a short description for project"}
+            live={"https://github.com/taraksh01/pet-pooja"}
+            code={"https://github.com/taraksh01/pet-pooja"}
+          /> */}
+        </div>
+      </div>
+    </main>
   );
 };
 
