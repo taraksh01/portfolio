4729cec360afd1209f812c87e4bfc17103336d71
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-10-22 11:36:05 +0530

    Changed styling and made ui responsive for all devices

--- src/Components/SingleProject.js
+++ src/Components/SingleProject.js
@@ -1,18 +1,20 @@
 const Project = ({ tech, name, desc, live, code }) => {
   return (
     <>
-      <div className="">
-        <img className="h-64 w-full rounded-xl" src="" alt="" />
-        <p className="text-2xl font-semibold p-1">{name}</p>
-        <p className="px-1">{tech.join(", ")}</p>
-        <p className="px-1">{desc}</p>
-        <div className="flex gap-4 px-1 py-2">
-          <button className="md:text-lg border border-orange-300 py-1 px-2 rounded-md">
-            live {"🚀"}
-          </button>
-          <button className="md:text-lg border border-orange-300 py-1 px-2 rounded-md">
-            code {"</>"}
-          </button>
+      <div className="w-80 sm:w-96 flex flex-col flex-shrink-0">
+        <img className="h-56 sm:h-60 w-full rounded-xl" src="" alt="" />
+        <div className="">
+          <p className="text-2xl font-semibold p-1">{name}</p>
+          <p className="px-1">{tech.join(", ")}</p>
+          <p className="px-1">{desc}</p>
+          <div className="flex gap-4 px-1 py-2">
+            <button className="md:text-lg border border-orange-300 py-1 px-2 rounded-md">
+              live {"🚀"}
+            </button>
+            <button className="md:text-lg border border-orange-300 py-1 px-2 rounded-md">
+              code {"</>"}
+            </button>
+          </div>
         </div>
       </div>
     </>
