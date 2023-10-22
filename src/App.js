f6e189cafa6188c88970c8d729a171ae591cafcd
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-10-22 09:44:38 +0530

    Added about page via routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Projects from "./Components/Projects";
+import About from "./Components/About";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const App = () => {
@@ -22,7 +23,7 @@ const appConfig = createBrowserRouter([
     children: [
       { path: "/", element: <Home /> },
       { path: "/projects", element: <Projects /> },
-      // { path: "/about", element: <About /> },
+      { path: "/about", element: <About /> },
       // { path: "/contact", element: <Contact /> },
     ],
   },
