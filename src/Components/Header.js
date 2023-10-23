7fb68712d0b30eec3572b8b10c511e8c8bf59f42
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-10-23 15:17:22 +0530

    Added hamberger menu for mobile device

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
+import { BiMenuAltRight } from "react-icons/bi";
 
 const Header = () => {
   return (
-    <div className="sticky top-0 bg-black pb-1">
-      <header className="max-w-screen-xl h-16 mx-auto text-gray-200 flex items-end justify-between font-mono">
-        <Link to={"/"} className="hover:text-white active:text-white">
+    <div className="sticky top-0 bg-black">
+      <header className="max-w-screen-xl h-16 mx-auto text-gray-200 flex items-center justify-between font-mono">
+        <Link to={"/"} className="hover:text-white active:text-white mx-2">
           <h1 className="text-2xl font-semibold">Tarak Shaw</h1>
         </Link>
-        <nav className="flex gap-6">
+        <nav className="hidden sm:flex gap-6 mx-2">
           <Link to={"/"} className="hover:text-white active:text-white">
             <p className="text-xl">
               <span className="text-orange-400">#</span>home
@@ -29,6 +30,9 @@ const Header = () => {
             </p>
           </Link>
         </nav>
+        <button className="flex sm:hidden mx-2 text-4xl">
+          <BiMenuAltRight />
+        </button>
       </header>
     </div>
   );
