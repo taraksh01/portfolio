13a26bda370e206d6e469a1c17bb0413290a5033
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-13 18:40:08 +0530

    Created navItems array and showing the links

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,33 +6,33 @@ import { useState } from "react";
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
 
+  const navItems = [
+    { name: "home", url: "/" },
+    { name: "works", url: "/projects" },
+    { name: "about", url: "/about" },
+    { name: "contact", url: "/contact" },
+    { name: "hire-me", url: "/hire" },
+  ];
+
   return (
     <div className="sticky top-0 bg-black">
       <header className="max-w-screen-xl h-16 mx-auto text-gray-200 flex items-center justify-between font-mono">
         <Link to={"/"} className="hover:text-white active:text-white mx-2">
-          <h1 className="text-2xl font-semibold">Tarak Shaw</h1>
+          <h1 className="text-2xl font-semibold mx-4">Tarak Shaw</h1>
         </Link>
         <nav className="hidden sm:flex gap-6 mx-2">
-          <Link to={"/"} className="hover:text-white active:text-white">
-            <p className="text-xl">
-              <span className="text-orange-400">#</span>home
-            </p>
-          </Link>
-          <Link to={"/projects"} className="hover:text-white active:text-white">
-            <p className="text-xl cursor-pointer">
-              <span className="text-orange-400">#</span>works
-            </p>
-          </Link>
-          <Link to={"/about"} className="hover:text-white active:text-white">
-            <p className="text-xl">
-              <span className="text-orange-400">#</span>about
-            </p>
-          </Link>
-          <Link to={"/contact"} className="hover:text-white active:text-white">
-            <p className="text-xl">
-              <span className="text-orange-400">#</span>contacts
-            </p>
-          </Link>
+          {navItems?.map((item) => (
+            <Link
+              to={item?.url}
+              key={item?.name}
+              className="hover:text-white active:text-white"
+            >
+              <p className="text-xl">
+                <span className="text-orange-400">#</span>
+                {item?.name}
+              </p>
+            </Link>
+          ))}
         </nav>
         <button className="flex sm:hidden mx-2">
           {showMenu ? (
@@ -48,51 +48,28 @@ const Header = () => {
           )}
         </button>
       </header>
-      <Menu status={showMenu} setStatus={setShowMenu} />
+      <Menu status={showMenu} setStatus={setShowMenu} navItems={navItems} />
     </div>
   );
 };
 
-const Menu = ({ status, setStatus }) => {
+const Menu = ({ status, setStatus, navItems }) => {
   return (
     <div className={status ? "visible h-screen sm:hidden" : "hidden"}>
       <nav className="flex sm:hidden flex-col bg-black gap-10 mx-4 my-10 text-2xl text-gray-200">
-        <Link
-          to={"/"}
-          className="hover:text-white active:text-white"
-          onClick={() => setStatus(false)}
-        >
-          <p>
-            <span className="text-orange-400">#</span>home
-          </p>
-        </Link>
-        <Link
-          to={"/projects"}
-          className="hover:text-white active:text-white"
-          onClick={() => setStatus(false)}
-        >
-          <p>
-            <span className="text-orange-400">#</span>works
-          </p>
-        </Link>
-        <Link
-          to={"/about"}
-          className="hover:text-white active:text-white"
-          onClick={() => setStatus(false)}
-        >
-          <p>
-            <span className="text-orange-400">#</span>about
-          </p>
-        </Link>
-        <Link
-          to={"/contact"}
-          className="hover:text-white active:text-white"
-          onClick={() => setStatus(false)}
-        >
-          <p>
-            <span className="text-orange-400">#</span>contacts
-          </p>
-        </Link>
+        {navItems?.map((item) => (
+          <Link
+            to={item?.url}
+            key={item?.name}
+            onClick={() => setStatus(false)}
+            className="hover:text-white active:text-white"
+          >
+            <p>
+              <span className="text-orange-400">#</span>
+              {item?.name}
+            </p>
+          </Link>
+        ))}
       </nav>
     </div>
   );
