6954d439dc81331583d623cd32b3e240b4fe0e5f
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-13 19:50:20 +0530

    Added links for live and code

--- src/Components/SingleProject.js
+++ src/Components/SingleProject.js
@@ -1,23 +1,33 @@
-const Project = ({ tech, name, desc, live, code }) => {
+import { Link } from "react-router-dom";
+
+const Project = ({ tech, name, desc, live, code, image }) => {
   return (
-    <>
-      <div className="w-80 sm:w-96 flex flex-col flex-shrink-0">
-        <img className="h-56 sm:h-60 w-full rounded-xl" src="" alt="" />
-        <div className="">
-          <p className="text-2xl font-semibold p-1">{name}</p>
-          <p className="px-1">{tech.join(", ")}</p>
-          <p className="px-1">{desc}</p>
-          <div className="flex gap-4 px-1 py-2">
-            <button className="md:text-lg border border-orange-300 py-1 px-2 rounded-md">
-              live {"🚀"}
-            </button>
-            <button className="md:text-lg border border-orange-300 py-1 px-2 rounded-md">
-              code {"</>"}
-            </button>
-          </div>
+    <div className="w-full sm:w-96 flex flex-col flex-shrink-0">
+      <img className="h-56 sm:h-60 w-full rounded-xl" src={image} alt="" />
+      <div>
+        <p className="text-2xl font-semibold px-1 my-2">{name}</p>
+        <p className="px-1 text-justify my-1">{desc}</p>
+        <p className="px-1 text-justify my-1">
+          <span className="font-bold">Tech Stack:</span> {tech.join(", ")}
+        </p>
+        <div className="flex justify-evenly items-center m-4">
+          <Link
+            to={live}
+            target="_blank"
+            className="md:text-lg transition-all duration-300 border hover:border-orange-300 p-2 rounded-md"
+          >
+            live {"🚀"}
+          </Link>
+          <Link
+            to={code}
+            target="_blank"
+            className="md:text-lg transition-all duration-300 border hover:border-orange-300 p-2 rounded-md"
+          >
+            code {"</>"}
+          </Link>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
