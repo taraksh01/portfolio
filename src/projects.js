25b912721ef2ed06db4def456a177531107ab01b
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-13 18:54:48 +0530

    Added projects detail

---
+++ src/projects.js
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    projectName: "LinkMe",
+    description:
+      "Linkme is a social connection web aap that lets user created and engage in posts.",
+    techStack: ["React.js", "Tailwind CSS", "Redux JS", "Appwrite"],
+    live: "https://link-me-gamma.vercel.app/",
+    code: "https://github.com/taraksh01/linkMe",
+    image: "",
+  },
+  {
+    projectName: "PetPooja",
+    description:
+      "PetPooja is a online food ordering web app that shows restaurants nearby user's location.",
+    techStack: ["React.js", "Tailwind CSS", "Redux JS"],
+    live: "https://petpooja.vercel.app/",
+    code: "https://github.com/taraksh01/petPooja",
+    image: "",
+  },
+];
