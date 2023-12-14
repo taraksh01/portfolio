f4d2b2ae5ae2546d6c3db6eafa8cfe4d29475537
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-14 17:13:56 +0530

    Wrapped image inside an div

--- src/Components/SingleProject.js
+++ src/Components/SingleProject.js
@@ -3,15 +3,13 @@ import { Link } from "react-router-dom";
 const Project = ({ tech, name, desc, live, code, image }) => {
   return (
     <div className="w-full sm:w-96 flex flex-col flex-shrink-0">
-      <img
-        className="h-56 sm:h-60 w-full rounded-xl"
-        src={image}
-        alt="project image"
-      />
+      <div className="w-full">
+        <img className="object-contain" src={image} alt="project image" />
+      </div>
       <div>
         <p className="text-2xl font-semibold px-1 my-2">{name}</p>
-        <p className="mx-1 leading-tight text-justify my-2 text-xl">{desc}</p>
-        <p className="mx-1 leading-tight text-justify my-2 text-xl">
+        <p className="mx-1 my-2 text-xl">{desc}</p>
+        <p className="mx-1 my-2 text-xl">
           <span className="font-bold">Tech Stack:</span> {tech.join(", ")}
         </p>
         <div className="flex justify-evenly items-center m-4">
