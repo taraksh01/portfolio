139299bb341be63d85d94a163b626bebf20db3fc
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-15 14:14:56 +0530

    Added skills as an array of objects

---
+++ src/skills.js
@@ -0,0 +1,7 @@
+export const skills = [
+  { heading: "Languages", fills: ["JavaScript", "Python"] },
+  { heading: "Tools", fills: ["Linux", "VSCode", "Git", "Github"] },
+  { heading: "Framework/Library", fills: ["React", "Redux", "tailwind"] },
+  { heading: "Databases", fills: ["MySQL"] },
+  { heading: "Others", fills: ["HTML", "CSS"] },
+];
