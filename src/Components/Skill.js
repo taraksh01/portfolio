bece25bb07675e7acd743eac82985cd7223b9f89
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-15 13:29:47 +0530

    Changed hover color on skills

--- src/Components/Skill.js
+++ src/Components/Skill.js
@@ -1,6 +1,6 @@
 const Skill = ({ heading, fills }) => {
   return (
-    <table className="border w-48 hover:text-orange-100">
+    <table className="border w-48 hover:text-orange-400">
       <tbody>
         <tr>
           <th className="border text-lg p-0.5">{heading}</th>
