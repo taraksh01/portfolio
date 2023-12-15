5d5cbba6c0b75f88bcb9f7c3d5b35f4ceacb8f90
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-15 21:14:41 +0530

    Added large and mini project details

--- src/projects.js
+++ src/projects.js
@@ -1,11 +1,16 @@
 import linkMe from "./assets/images/linkMe.png";
+import petPooja from "./assets/images/petPooja.png";
+import passwordGenerator from "./assets/images/passwordGenerator.png";
+import currencyConverter from "./assets/images/currencyConverter.png";
+import ticTacToe from "./assets/images/ticTacToe.png";
+import rockPaperScissors from "./assets/images/rockPaperScissors.png";
 
 export const projects = [
   {
     projectName: "LinkMe",
     description:
-      "Linkme is a social connection web aap that lets user created and engage in posts.",
-    techStack: ["React.js", "Tailwind CSS", "Redux JS", "Appwrite"],
+      "Linkme is a social connection web aap that let you share your thoghts with other user.",
+    techStack: ["React.js", "Redux JS", "Tailwind CSS", "Appwrite"],
     live: "https://link-me-gamma.vercel.app/",
     code: "https://github.com/taraksh01/linkMe",
     image: linkMe,
@@ -13,10 +18,70 @@ export const projects = [
   {
     projectName: "PetPooja",
     description:
-      "PetPooja is a online food ordering web app that shows restaurants nearby user's location.",
-    techStack: ["React.js", "Tailwind CSS", "Redux JS"],
+      "PetPooja is a online food ordering web app that shows restaurants near your location.",
+    techStack: ["React.js", "Redux JS", "Tailwind CSS"],
     live: "https://petpooja.vercel.app/",
     code: "https://github.com/taraksh01/petPooja",
-    image: "",
+    image: petPooja,
+  },
+];
+
+export const largeProjects = [
+  {
+    projectName: "LinkMe",
+    description:
+      "Linkme is a social connection web aap that let you share your thoghts with other user.",
+    techStack: ["React.js", "Redux JS", "Tailwind CSS", "Appwrite"],
+    live: "https://link-me-gamma.vercel.app/",
+    code: "https://github.com/taraksh01/linkMe",
+    image: linkMe,
+  },
+  {
+    projectName: "PetPooja",
+    description:
+      "PetPooja is a online food ordering web app that shows restaurants near your location.",
+    techStack: ["React.js", "Redux JS", "Tailwind CSS"],
+    live: "https://petpooja.vercel.app/",
+    code: "https://github.com/taraksh01/petPooja",
+    image: petPooja,
+  },
+];
+
+export const miniProjects = [
+  {
+    projectName: "Password Generator",
+    description:
+      "Password Generator generates a strong password to keep you secure online",
+    techStack: ["React.js", "Tailwind CSS"],
+    live: "https://password-generator-tarksh01.vercel.app/",
+    code: "https://github.com/taraksh01/passwordGenerator",
+    image: passwordGenerator,
+  },
+  {
+    projectName: "Currency Converter",
+    description:
+      "Currency Converter converts currency using real time exchange rates",
+    techStack: ["React.js", "Tailwind CSS"],
+    live: "https://currency-converter-taraksh01.vercel.app/",
+    code: "https://github.com/taraksh01/currencyConverter/",
+    image: currencyConverter,
+  },
+  {
+    projectName: "Tic Tac Toe",
+    description:
+      "Tic Tac Toe is a classic game that lets you play a match with your loved one.",
+    techStack: ["HTML", "CSS", "JavaScript"],
+    live: "https://tic-tac-toe-tarksh01.vercel.app/",
+    code: "https://github.com/taraksh01/tic-tac-toe/",
+    image: ticTacToe,
+  },
+  {
+    projectName: "Rock Paper Scissors",
+    description:
+      "Rock Paper Scissors is a classic game that lets you play a match agains computer.",
+    techStack: ["HTML", "CSS", "JavaScript"],
+    live: "https://rock-paper-scissor-taraksh01.vercel.app/",
+    code: "https://github.com/taraksh01/rock-paper-scissor/",
+    image: rockPaperScissors,
   },
 ];
