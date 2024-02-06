bbaf2e653e3e9803ab829b37bb1f8d132a1a6312
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-02-06 17:00:03 +0530

    Update skills

--- src/skills.js
+++ src/skills.js
@@ -1,7 +1,33 @@
 export const skills = [
-  { heading: "Languages", fills: ["JavaScript", "Python"] },
-  { heading: "Tools", fills: ["Linux", "VSCode", "Git", "Github"] },
-  { heading: "Framework/Library", fills: ["React", "Redux", "tailwind"] },
-  { heading: "Databases", fills: ["MySQL"] },
+  { heading: "Languages", fills: ["JavaScript", "Python", "Java"] },
+  {
+    heading: "Tools",
+    fills: [
+      "Linux",
+      "VSCode",
+      "Git",
+      "Github",
+      "Chrome Developer Tools",
+      "Figma",
+      "Netlify",
+      "Vercel",
+      "JWT",
+      "Cloudinary",
+      "RestAPI",
+      "Multer",
+    ],
+  },
+  {
+    heading: "Framework/Library",
+    fills: [
+      "React.JS",
+      "React-Redux",
+      "tailwind",
+      "Node.JS",
+      "Express.JS",
+      "Appwrite",
+    ],
+  },
+  { heading: "Databases", fills: ["MySQL", "MongoDB"] },
   { heading: "Others", fills: ["HTML", "CSS"] },
 ];
